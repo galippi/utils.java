613e67bf418ffc6523a801380655bcb550c86be9
Gabor Liptak <liptakgabor@freemail.hu>
2018-12-09 20:04:45 +0100

    bin class is extended: - getInt - getInt32 - setInt32 - getInt16 - setInt16 - getUint16 - setUint16

--- bin.java
+++ bin.java
@@ -14,4 +14,42 @@ public class bin {
   {
     return ((int)b & 0xFF);
   }
+  static public int getInt(byte data)
+  {
+    return (int)data & 0xFF;
+  }
+  static public int getInt32(byte[] data, int offset)
+  {
+    return ( getInt(data[offset + 0]) +
+            (getInt(data[offset + 1]) * 256) +
+            (getInt(data[offset + 2]) * 655356) +
+            (getInt(data[offset + 3]) * 16777216) );
+  }
+  static public void setInt32(byte[] buf, int offset, int data)
+  {
+    buf[offset + 0] = (byte)(data & 0xFF);
+    buf[offset + 1] = (byte)((data / 256) & 0xFF);
+    buf[offset + 2] = (byte)((data / 65536) & 0xFF);
+    buf[offset + 3] = (byte)((data / 16777216) & 0xFF);
+  }
+  static public int getInt16(byte[] data, int offset)
+  {
+    return ( getInt(data[offset + 0]) +
+            (( (int)data[offset + 1]) * 256));
+  }
+  static public void setInt16(byte[] buf, int offset, int data)
+  {
+    buf[offset + 0] = (byte)(data & 0xFF);
+    buf[offset + 1] = (byte)((data / 256) & 0xFF);
+  }
+  static public int getUint16(byte[] data, int offset)
+  {
+    return ( getInt(data[offset + 0]) +
+            (getInt(data[offset + 1]) * 256));
+  }
+  static public void setUint16(byte[] buf, int offset, int data)
+  {
+    buf[offset + 0] = (byte)(data & 0xFF);
+    buf[offset + 1] = (byte)((data / 256) & 0xFF);
+  }
 }
