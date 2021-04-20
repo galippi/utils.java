56a55a904884a5240d94167aadf1e5f4a8273a5b
Gabor Liptak <liptakgabor@freemail.hu>
2021-04-20 14:55:09 +0200

    TopWatch class is created

---
+++ StopWatch.java
@@ -0,0 +1,45 @@
+package utils;
+
+public class StopWatch {
+    long t0;
+    long dt = -1;
+
+    public StopWatch()
+    {
+        start();
+    }
+
+    public void start()
+    {
+        t0 = System.nanoTime();
+        dt = -1;
+    }
+
+    public void stop()
+    {
+        dt = System.nanoTime() - t0;
+    }
+
+    public long elapsedTime_ns()
+    {
+        if (dt < 0)
+            return System.nanoTime() - t0;
+        else
+            return dt;
+    }
+
+    public long elapsedTime_us()
+    {
+        return (elapsedTime_ns() + 500) / 1000;
+    }
+
+    public int elapsedTime_ms()
+    {
+        return (int)((elapsedTime_us() + 500) / 1000);
+    }
+
+    public double elapsedTime()
+    { // in ms
+        return elapsedTime_ns() / 1000000.0;
+    }
+}
