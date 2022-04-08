cc64048fe41185285e319ecd196ed3dc52aeaca4
Gabor Liptak <liptakgabor@freemail.hu>
2022-04-08 08:07:37 +0200

    The package is changed to lippiWare.utils

--- dbg.java
+++ dbg.java
@@ -1,16 +1,10 @@
-package utils;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Gabor Liptak
  */
-//import java.io.*;
+
+package lippiWare.utils;
+
 import utils.Sprintf;
 
 public class dbg {
