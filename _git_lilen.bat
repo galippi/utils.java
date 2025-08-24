98f452ff79b3ebca5ad55c9d15b7ab6226a55c09
Gabor Liptak <liptakgabor@freemail.hu>
2025-08-24 09:11:41 +0200

    UnitTest environment for bin module is prepared

--- _git_lilen.bat
+++ _git_lilen.bat
@@ -1,4 +1,13 @@
+@echo off
 ::set path=C:\Programs\Git\bin\;%PATH%
 @set path=C:\Programs\cygwin\bin\;%PATH%
 @set HOME=C:\Users\liptakok\Gabor
+
+if "%*" == "" goto noparam
+%*
+goto end
+
+:noparam
 start "%CD%" cmd
+
+:end
